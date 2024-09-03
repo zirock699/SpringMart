6e716e0bc58a95b710cafa62c872dcdc9b39c67b
zirock699 <57227114+zirock699@users.noreply.github.com>
2024-09-03 17:45:31 +0100

    v2

--- src/main/java/com/example/springmart/service/PaymentService.java
+++ src/main/java/com/example/springmart/service/PaymentService.java
@@ -4,7 +4,7 @@ public class PaymentService {
     private static final String STRIPE_API_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
 
     public void processPayment() {
-        // Use the API key directly in code
+
         Stripe.apiKey = STRIPE_API_KEY;
         // Payment processing logic...
     }
--- src/main/java/com/example/springmart/service/Stripe.java
+++ src/main/java/com/example/springmart/service/Stripe.java
@@ -2,4 +2,5 @@ package com.example.springmart.service;
 
 public class Stripe {
     public static String apiKey;
+    public static String id;
 }
